795c841becccdb9e738d16ad5e43f78398947b14
DESKTOP-B68R1AV\setby <itcrowd91@gmail.com>
2021-01-08 13:03:59 +0900

    초기환경설정 spring-mybatis 연결작업

---
+++ team/src/main/java/com/project/domain/QuestionVO.java
@@ -0,0 +1,13 @@
+package com.project.domain;
+
+import java.util.Date;
+
+public class QuestionVO {
+	  private Long bno;
+	  private String title;
+	  private String message;
+	  private String name;
+	  private String email;
+	  private Date regdate;
+
+}
---
+++ team/src/main/java/com/project/mapper/QuestionMapper.java
@@ -0,0 +1,13 @@
+package com.project.mapper;
+
+import java.util.List;
+
+import com.project.domain.QuestionVO;
+
+
+
+public interface QuestionMapper {
+
+	
+	public List<QuestionVO> getList();
+}
